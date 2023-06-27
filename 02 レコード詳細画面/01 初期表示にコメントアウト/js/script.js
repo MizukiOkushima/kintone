7b89e33249bd5cff0bbed60660a0a3821fe04ce5
MizukiOkushima <caresupplement@icloud.com>
2023-06-27 20:20:35 +0900

    2023-06-27

--- 02 レコード詳細画面/01 初期表示にコメントアウト/js/script.js
+++ 02 レコード詳細画面/01 初期表示にコメントアウト/js/script.js
@@ -14,8 +14,12 @@
   kintone.events.on(events_show, async (event) => {
 
     //コメント入力領域をページ読み込み時に非表示
-    document.getElementsByClassName('gaia-argoui-app-show-sidebar-dragged')[0].style.display = "none";
-    location.replace(document.URL + "&tab=none");
+    const tabNone = "&tab=none";
+    if (document.URL.indexOf(tabNone) == -1) {
+        document.getElementsByClassName('gaia-argoui-app-show-sidebar-dragged')[0].style.display = "none";
+        location.replace(document.URL + "&tab=none");
+    }
+
   });
 
 }());
