559296fb2579ed9c7a2fa7d9850aa1071dd2f04d
MizukiOkushima <caresupplement@icloud.com>
2023-09-16 18:14:01 +0900

    2023-09-16

--- 99 共通/002 非活性処理/JS/script.js
+++ 99 共通/002 非活性処理/JS/script.js
@@ -1,27 +1,76 @@
 (function () {
   "use strict";
 
-  //作成イベント
+  //レコード作成・編集イベント
   const events_create = [
-      'app.record.create.show',
-      'app.record.edit.show'
+    'app.record.create.show',
+    'app.record.edit.show'
   ];
 
-  kintone.events.on(events_create, async (event) => {
-    // フィールド名の項目を非活性にする
-    event.record["フィールド名"].disabled = true;
+  // 一覧編集イベント
+  const events_index_edit = [
+    'app.record.index.edit.show'
+  ];
 
-    // テーブルのフィールドを全件非活性
-    for (let i = 0; i < event.record["テーブル名"].value.length; i++) {
-      event.record["テーブル名"].value[i].value["フィールド名"].disabled = true;
+  // 汎用エラーメッセージ
+  const COMMON_ERROR_MESSAGE = 'エラーが発生しました。システム担当者に連絡してください。';
+
+  //
+  // レコード作成・編集イベント
+  //
+  kintone.events.on(events_create, async (event) => {
+
+    try {
+
+      // フィールド名の項目を非活性にする
+      event.record["フィールド名"].disabled = true;
+
+      // テーブルのフィールドを全件非活性
+      for (let i = 0; i < event.record["テーブル名"].value.length; i++) {
+        event.record["テーブル名"].value[i].value["フィールド名"].disabled = true;
+      }
+
+      // テーブルのフィールドを全件非活性(forEachで書く場合)
+      event.record["テーブル名"].value.forEach(function (row) {
+        // 各請求予定日フィールドの非活性
+        row.value["フィールド名"].disabled = true;
+      });
+
+      return event;
+
+    } catch (error) {
+
+      console.log(error);
+      event.error = COMMON_ERROR_MESSAGE;
+
+      return event;
+    
     }
 
-    // テーブルのフィールドを全件非活性(forEachで書く場合)
-    event.record["テーブル名"].value.forEach(function (row) {
-      // 各請求予定日フィールドの非活性
-      row.value["フィールド名"].disabled = true;
-    });
+  });
 
-  })
+
+  //
+  // 一覧編集イベント
+  // 
+  kintone.events.on(events_index_edit, (event) => {
+
+    try {
+
+      // フィールド名の項目を比活性にする
+      event.record['フィールド'].disabled = true;
+
+      return event;
+
+    } catch (error) {
+
+      console.log(error);
+      event.error = COMMON_ERROR_MESSAGE;
+
+      return event;
+      
+    }
+
+  });
 
 })();
