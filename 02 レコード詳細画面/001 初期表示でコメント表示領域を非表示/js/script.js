9e94baf1fc551ae445e8cda5b5ccaf7477b64c75
MizukiOkushima <95268598+MizukiOkushima@users.noreply.github.com>
2023-12-14 13:00:18 +0900

    Update script.js

--- 02 レコード詳細画面/001 初期表示でコメント表示領域を非表示/js/script.js
+++ 02 レコード詳細画面/001 初期表示でコメント表示領域を非表示/js/script.js
@@ -14,12 +14,34 @@
   kintone.events.on(events_show, async (event) => {
 
     //コメント入力領域をページ読み込み時に非表示
-    const tabNone = "&tab=none";
-    if (document.URL.indexOf(tabNone) == -1) {
-        document.getElementsByClassName('gaia-argoui-app-show-sidebar-dragged')[0].style.display = "none";
-        location.replace(document.URL + "&tab=none");
-    }
+    hiddenSidebar();
 
   });
+
+  // 関数 コメント入力領域を非表示
+  function hiddenSidebar() {
+
+      const url = window.location;
+
+      // 本来URLSearchParamsはURLパラメーターに使用するが、'='と'&'の判定が容易なため使用
+      // →ただしURLハッシュの先頭の文字'#'は無視されないため、URLハッシュの先頭の1文字目の除外処理をする
+      const params = new URLSearchParams(url.hash.slice(1));
+      const paraName = 'tab';
+      const paraValue = 'none';
+
+      // リロード対策。既に'tab'パラメーターが存在していれば何もしない
+      if (params.has(paraName)) {
+          return;
+      }
+
+      // サイドバーのスタイル変更
+      const sidebar = document.getElementsByClassName('gaia-argoui-app-show-sidebar-dragged')[0];
+      sidebar.style.display = paraValue;
+
+      // 'tab'パラメータをセットして、URLハッシュを更新する
+      params.set(paraName, paraValue);
+      url.hash = params.toString();
+
+  }
 
 }());
